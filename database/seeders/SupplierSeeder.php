d690d485592ce2886d0d77902eb3952335f976ae
Толстоухов Александр Петрович <ykt94@yandex.ru>
2022-05-18 12:40:41 +0900

    initial commit

---
+++ database/seeders/SupplierSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Supplier;
+
+class SupplierSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Supplier::factory(6)->create();
+    }
+}
